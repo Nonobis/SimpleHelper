721a78f094723e45a207826675f9c7075044e918
Arnaud Dartois <nonobis@gmail.com>
2018-04-20 13:00:10 +0200

    Add LocalComputerHelper

---
+++ SimpleHelper.Core/LocalComputerHelper.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Runtime.InteropServices;
+using System.Text;
+
+namespace SimpleHelper.Core
+{
+    public static class LocalComputerHelper
+    {
+        /// <summary>
+        /// Geteuids this instance.
+        /// </summary>
+        /// <returns></returns>
+        [DllImport("libc")] private static extern uint geteuid();
+
+        /// <summary>
+        /// Gets the user temporary path.
+        /// </summary>
+        /// <returns></returns>
+        public static string GetUserTempPath()
+        {
+            string path = Path.GetTempPath();
+            if (RuntimeInformation.IsOSPlatform(OSPlatform.Linux))
+            {
+                path = Path.Combine(path, geteuid().ToString());
+                if (!Directory.Exists(path))
+                    Directory.CreateDirectory(path);
+            }
+            return path;
+        }
+    }
+}
