4344093f80fc462a560dc171af31c80b58d6e4cf
Arnaud Dartois <nonobis@gmail.com>
2018-05-11 10:58:12 +0200

    Refactoring and adding method in FileHelper

--- SimpleHelper.Core/FileHelper.cs
+++ SimpleHelper.Core/FileHelper.cs
@@ -1,4 +1,5 @@
-﻿using System.IO;
+﻿using System;
+using System.IO;
 using System.Threading.Tasks;
 
 namespace SimpleHelper.Core
@@ -9,10 +10,10 @@ namespace SimpleHelper.Core
     /// Calculates a 32bit Cyclic Redundancy Checksum (CRC) using the
     /// same polynomial used by Zip.
     /// </summary>
-    public class CRC32
+    public class Crc32
     {
-        uint[] crc32Table;
-        const int BUFFER_SIZE = 1024;
+        private readonly uint[] _crc32Table;
+        private const int BufferSize = 1024;
 
         /// <summary>
         /// Returns the CRC32 for the specified stream.
@@ -23,17 +24,16 @@ namespace SimpleHelper.Core
         {
             unchecked
             {
-                uint crc32Result;
-                crc32Result = 0xFFFFFFFF;
-                byte[] buffer = new byte[BUFFER_SIZE];
-                int readSize = BUFFER_SIZE;
+                var crc32Result = 0xFFFFFFFF;
+                var buffer = new byte[BufferSize];
+                var readSize = BufferSize;
 
-                int count = stream.Read(buffer, 0, readSize);
+                var count = stream.Read(buffer, 0, readSize);
                 while (count > 0)
                 {
-                    for (int i = 0; i < count; i++)
+                    for (var i = 0; i < count; i++)
                     {
-                        crc32Result = ((crc32Result) >> 8) ^ crc32Table[(buffer[i]) ^
+                        crc32Result = ((crc32Result) >> 8) ^ _crc32Table[(buffer[i]) ^
                          ((crc32Result) & 0x000000FF)];
                     }
                     count = stream.Read(buffer, 0, readSize);
@@ -47,19 +47,18 @@ namespace SimpleHelper.Core
         /// Construct an instance of the CRC32 class, pre-initialising the table
         /// for speed of lookup.
         /// </summary>
-        public CRC32()
+        public Crc32()
         {
             unchecked
             {
                 // This is the official polynomial used by CRC32 in PKZip.
                 // Often the polynomial is shown reversed as 0x04C11DB7.
-                uint dwPolynomial = 0xEDB88320;
+                const uint dwPolynomial = 0xEDB88320;
                 uint i, j;
-                crc32Table = new uint[256];
-                uint dwCrc;
+                _crc32Table = new uint[256];
                 for (i = 0; i < 256; i++)
                 {
-                    dwCrc = i;
+                    var dwCrc = i;
                     for (j = 8; j > 0; j--)
                     {
                         if ((dwCrc & 1) == 1)
@@ -71,7 +70,7 @@ namespace SimpleHelper.Core
                             dwCrc >>= 1;
                         }
                     }
-                    crc32Table[i] = dwCrc;
+                    _crc32Table[i] = dwCrc;
                 }
             }
         }
@@ -102,16 +101,16 @@ namespace SimpleHelper.Core
         {
             try
             {
-                foreach (DirectoryInfo dir in pSource.GetDirectories())
+                foreach (var dir in pSource.GetDirectories())
                 {
-                    DirectoryInfo targetSubDirectory = pTarget.CreateSubdirectory(dir.Name);
+                    var targetSubDirectory = pTarget.CreateSubdirectory(dir.Name);
                     CopyFilesRecursively(dir, targetSubDirectory, pDeleteExistingTarget);
                 }
 
                 Parallel.ForEach(pSource.GetFiles(),
                 filePath =>
                 {
-                    Task task = Task.Factory.StartNew(() =>
+                    Task.Factory.StartNew(() =>
                     {
                         if (File.Exists(Path.Combine(pTarget.FullName, filePath.Name)))
                             File.Delete(Path.Combine(pTarget.FullName, filePath.Name));
@@ -129,16 +128,61 @@ namespace SimpleHelper.Core
         }
 
         /// <summary>
+        /// Get file content as Base64
+        /// </summary>
+        /// <param name="pFilePath"></param>
+        /// <returns></returns>
+        public static string FileToBase64(string pFilePath)
+        {
+            var sValue = string.Empty;
+            var oFile = new FileInfo(pFilePath);
+            if (!oFile.Exists)
+                return sValue;
+
+            var fs = new FileStream(oFile.FullName,
+                FileMode.Open,
+                FileAccess.Read);
+            var filebytes = new byte[fs.Length];
+            fs.Read(filebytes, 0, Convert.ToInt32(fs.Length));
+            sValue = Convert.ToBase64String(filebytes);
+            return sValue;
+        }
+
+        /// <summary>
+        /// Write file from base64 string
+        /// </summary>
+        /// <param name="pData"></param>
+        /// <param name="psFilePath"></param>
+        /// <param name="pDeleteIfExisting"></param>
+        public static void FromBase64ToFile(string pData, string psFilePath, bool pDeleteIfExisting)
+        {
+            if (string.IsNullOrEmpty(pData))
+                return;
+
+            if (File.Exists(pData) && pDeleteIfExisting)
+                File.Delete(psFilePath);
+
+            var filebytes = Convert.FromBase64String(pData);
+            using (var fs = new FileStream(psFilePath,
+                FileMode.CreateNew,
+                FileAccess.Write,
+                FileShare.None))
+            {
+                fs.Write(filebytes, 0, filebytes.Length);
+            }
+        }
+
+        /// <summary>
         /// Gets the CRC of file.
         /// </summary>
         /// <param name="psFile">The file path.</param>
         /// <returns></returns>
-        public static long GetCRCFile(string psFile)
+        public static long GetCrcFile(string psFile)
         {
             using (var streamFile = new FileStream(psFile, FileMode.Open))
             {
-                var crcFile = new CRC32();
-                return crcFile.GetCrc32(streamFile as Stream);
+                var crcFile = new Crc32();
+                return crcFile.GetCrc32(streamFile);
             }
         }
     }
