2e5fb45040a0e6b501b0bc34e60e38b0b6cf0eb8
Chirag Bhatt <chiragbhattsvkd@gmail.com>
2020-11-18 21:35:18 +0530

    Added JSON helper

---
+++ SimpleHelper/JsonHelper.cs
@@ -0,0 +1,28 @@
+﻿using Newtonsoft.Json;
+
+namespace SimpleHelper
+{
+    public static class JsonHelper
+    {
+        /// <summary>
+        /// Will Convert a json string to object.
+        /// </summary>
+        /// <typeparam name="T">Type of Json to return</typeparam>
+        /// <param name="json">Json String</param>
+        /// <returns>The object Deserialized.</returns>
+        public static T ToJsonObject<T>(this string json)
+        {
+            return JsonConvert.DeserializeObject<T>(json);
+        }
+
+        /// <summary>
+        /// Will convert a object, to json string.
+        /// </summary>
+        /// <param name="obj">The object to convert.</param>
+        /// <returns>Json string serialized.</returns>
+        public static string ToJsonString(this object obj)
+        {
+            return JsonConvert.SerializeObject(obj);
+        }
+    }
+}
